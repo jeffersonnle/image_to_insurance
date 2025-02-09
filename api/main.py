5e227c0b66730b6880e493cf07f181df31ebe60f
Soohyeuk <csh040816@gmail.com>
2025-02-08 20:57:59 -0500

    something reallllllly wrong

--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ from typing import List
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-from test import analyze_image
+from image_to_list.image_to_list.test import analyze_image
 
 ##### configs #####
 app = FastAPI()
